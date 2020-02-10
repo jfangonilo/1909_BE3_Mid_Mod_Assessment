f02eef14d0f26dca1c473c33cf75ae504b167b8d
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-10 09:45:47 -0700

    Add character poro

---
+++ app/models/character.rb
@@ -0,0 +1,10 @@
+class Character
+  attr_reader :name, :role, :house, :patronus
+
+  def initialize(data)
+    @name = data[:name]
+    @role = data[:role]
+    @house = data[:house]
+    @patronus = data[:patronus]
+  end
+end
