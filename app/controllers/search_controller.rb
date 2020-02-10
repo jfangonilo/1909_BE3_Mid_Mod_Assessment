8a63c5680d366ce015a4b8027b8c85e0733e000f
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-10 10:48:38 -0700

    Remove commented code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,20 +3,5 @@ class SearchController < ApplicationController
     render locals: {
       facade: Facade.new(params[:house])
     }
-
-    # house = params[:house]
-    # conn = Faraday.new("https://www.potterapi.com/v1/") do |f|
-    #   f.adapter Faraday.default_adapter
-    #   f.params['key'] = ENV['HP_API_KEY']
-    #   f.params['house'] = params[:house]
-    #   f.params['orderOfThePhoenix'] = true
-    # end
-
-    # response = conn.get("characters")
-
-    # member_list = JSON.parse(response.body, symbolize_names: true)
-    # @members = member_list.map do |data|
-    #   Character.new(data)
-    # end
   end
 end
