425af0e872c3a255919719b3b1bb1118fdaf24bf
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-10 10:44:25 -0700

    Add additional params to index action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,22 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new("https://www.potterapi.com/v1/") do |f|
-      f.adapter Faraday.default_adapter
-      f.params['key'] = ENV['HP_API_KEY']
-    end
-    response = conn.get("characters")
+    render locals: {
+      facade: Facade.new(params)
+    }
 
-    member_list = JSON.parse(response.body, symbolize_names: true)
-    @members = member_list.map do |data|
-      Character.new(data)
-    end
+    # house = params[:house]
+    # conn = Faraday.new("https://www.potterapi.com/v1/") do |f|
+    #   f.adapter Faraday.default_adapter
+    #   f.params['key'] = ENV['HP_API_KEY']
+    #   f.params['house'] = params[:house]
+    #   f.params['orderOfThePhoenix'] = true
+    # end
+
+    # response = conn.get("characters")
+
+    # member_list = JSON.parse(response.body, symbolize_names: true)
+    # @members = member_list.map do |data|
+    #   Character.new(data)
+    # end
   end
 end
