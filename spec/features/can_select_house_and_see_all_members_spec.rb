0e3afbdf2e9281a200e55859773317c30c504a6a
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-10 10:48:11 -0700

    Add check for sections

--- spec/features/can_select_house_and_see_all_members_spec.rb
+++ spec/features/can_select_house_and_see_all_members_spec.rb
@@ -10,5 +10,11 @@ feature "search for members by house and select Gryffindor" do
     expect(current_path).to eq "/search"
 
     expect(page).to have_css('.members', count: 21)
+    within(first('.members')) do
+      expect(page).to have_css('.name')
+      expect(page).to have_css('.house')
+      expect(page).to have_css('.role')
+      expect(page).to have_css('.patronus')
+    end
   end
 end
